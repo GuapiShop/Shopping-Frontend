edfdc8cd329f40acffd785a399e021c7ce5ab3cb
dennis-gomez <dennis.gomez1310@gmail.com>
2025-12-10 19:36:30 -0600

    Main page version 2
    Include delivery, mobile version and technical support information

--- Shooping-Frontend/src/components/templates/templateMainPage.tsx
+++ Shooping-Frontend/src/components/templates/templateMainPage.tsx
@@ -18,55 +18,95 @@ const TemplateMainPage: React.FC = () => {
   };
 
   return (
-    <div className="flex space-x-4 mx-auto mb-4 justify-center">
-        {/*Left Image 1*/}
-        <img 
-            src="/carousel-img2.jpg" 
-            alt="Main Visual" 
-            className="h-12 min-h-70 min-w-80" 
-        />
-      
-        {/* Carousel */}
-        <div 
-            className="w-97 relative overflow-hidden"
-        >
-            
-            {/* Slides container */}
-            <div
-                className="flex transition-transform duration-500"
-                style={{ transform: `translateX(-${current * 100}%)` }}
+    <div className="py-5">
+        <div className="flex space-x-4 mx-auto mb-4 justify-center pb-15">
+            {/*Left Image 1*/}
+            <img 
+                src="/carousel-img2.jpg" 
+                alt="Main Visual" 
+                className="h-12 min-h-70 min-w-80" 
+            />
+        
+            {/* Carousel */}
+            <div 
+                className="w-97 relative overflow-hidden"
             >
-                {images.map((img, index) => (
-                    <img
-                        key={index}
-                        src={img}
-                        className="w-3xl h-70 object-cover"
-                    />
-                ))}
+                
+                {/* Slides container */}
+                <div
+                    className="flex transition-transform duration-500"
+                    style={{ transform: `translateX(-${current * 100}%)` }}
+                >
+                    {images.map((img, index) => (
+                        <img
+                            key={index}
+                            src={img}
+                            className="w-3xl h-70 object-cover"
+                        />
+                    ))}
+                </div>
+
+                {/* Buttons */}
+                <button
+                    onClick={prevSlide}
+                    className="absolute left-2 top-1/2 -translate-y-1/2 bg-white/70 px-3 py-2 rounded-full"
+                >
+                    ❮
+                </button>
+
+                <button
+                    onClick={nextSlide}
+                    className="absolute right-2 top-1/2 -translate-y-1/2 bg-white/70 px-3 py-2 rounded-full"
+                >
+                    ❯
+                </button>
             </div>
 
-            {/* Buttons */}
-            <button
-                onClick={prevSlide}
-                className="absolute left-2 top-1/2 -translate-y-1/2 bg-white/70 px-3 py-2 rounded-full"
-            >
-                ❮
-            </button>
-
-            <button
-                onClick={nextSlide}
-                className="absolute right-2 top-1/2 -translate-y-1/2 bg-white/70 px-3 py-2 rounded-full"
-            >
-                ❯
-            </button>
+            {/*Right Image*/}
+            <img 
+                src="/carousel-img3.jpg" 
+                alt="Main Visual" 
+                className="h-12 min-h-70 min-w-80" 
+            />
         </div>
 
-        {/*Right Image*/}
-        <img 
-            src="/carousel-img3.jpg" 
-            alt="Main Visual" 
-            className="h-12 min-h-70 min-w-80" 
-        />
+        <div className="flex justify-center">
+            <div className="flex justify-center space-x-5 pr-10">
+                <img 
+                    src="/delevery.png" 
+                    alt="Main Visual" 
+                    className="h-12 min-h-20 min-w-20" 
+                />
+                <div className="text-lg text-left">
+                    <div>Delivery options available</div>
+                    <div>Choose your preferred method</div>
+                </div>
+            </div>
+            
+            <div className="flex justify-center pr-10">
+                <img 
+                    src="/phone.png" 
+                    alt="Main Visual" 
+                    className="h-12 min-h-20 min-w-20" 
+                />
+                <div className="text-lg text-left">
+                    <div>Our mobile version is ready</div>
+                    <div>Try our mobile version now!</div>
+                </div>
+            </div>
+
+            <div className="flex justify-center space-x-5 pr-10">
+                <img 
+                    src="/technical-support.png" 
+                    alt="Main Visual" 
+                    className="h-12 min-h-20 min-w-20" 
+                />
+                <div className="text-lg text-left">
+                    <div>Need assistance?</div>
+                    <div>Call us anytime!</div>
+                </div>
+            </div>
+        </div>
     </div>
   );
 };
