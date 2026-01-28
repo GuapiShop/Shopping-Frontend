d34b6f2db36563de98031bcc0822b8c84aba2aa9
dennis-gomez <dennis.gomez1310@gmail.com>
2026-01-28 12:56:08 -0600

    Sidebar Cart Shop
    Aggregation of component 'cartSidebar.tsx' and add to the 'mainHeader.tsx' component to see the shopping cart.

--- Shopping-Frontend/src/components/organisms/mainHeader.tsx
+++ Shopping-Frontend/src/components/organisms/mainHeader.tsx
@@ -1,29 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LinkGroup from '../molecules/linkGroup';
+import CartSidebar from './cartSidebar';
 import { useNavigate } from 'react-router-dom';
 import { logout } from '../../services/authService';
-import { ArrowLeftStartOnRectangleIcon } from "@heroicons/react/24/outline";
+import { ArrowLeftStartOnRectangleIcon, ShoppingCartIcon } from "@heroicons/react/24/outline";
 
 const MainHeader: React.FC = () => {
-
+  const [isOpen, setOpen] = useState<boolean>(false);
   const navigate = useNavigate();
 
   const handleLogoClick = () => {
     navigate('/main-page');
   } 
 
+  const handleCartClick = () => {
+    setOpen(!isOpen);
+  }
+
   return (
     <>
       {/*Image Logo*/}
-      <div className='flex bg-[#eb354c] p-5 mb-4'>  
-        <img 
-          className='h-12 mx-auto min-h-20 hover:cursor-pointer'
-          src="/guapishop.png" 
-          alt="Logo" 
-          onClick={
-            handleLogoClick
-          } 
-        />
+      <div className='flex bg-[#eb354c] p-5 mb-4'>
         <button
           title='Log Out'
           className="text-white mt-2 hover:cursor-pointer "
@@ -34,6 +31,25 @@ const MainHeader: React.FC = () => {
         >
           <ArrowLeftStartOnRectangleIcon className='h-10 w-9 text-white font-semibold' />
         </button>
+
+        <img 
+          className='h-12 mx-auto min-h-20 hover:cursor-pointer'
+          src="/guapishop.png" 
+          alt="Logo" 
+          onClick={handleLogoClick} 
+        />
+
+        {localStorage.getItem("role") == "client" && (
+          <button
+            title='Shopping Cart'
+            className="text-white mt-2 hover:cursor-pointer "
+            onClick={() => {
+              handleCartClick();
+            }}
+          >
+            <ShoppingCartIcon className='h-10 w-9 text-white font-semibold'/>
+          </button>
+        )}
       </div>
 
       {/*Group of Links*/}
@@ -42,6 +58,11 @@ const MainHeader: React.FC = () => {
       <div className='mx-30 py-4'>
         <hr className='text-gray-500 opacity-50'/>
       </div>
+
+      <CartSidebar  
+        isOpen={isOpen}
+        onClose={handleCartClick}
+      />
     </>
   );
 } 
